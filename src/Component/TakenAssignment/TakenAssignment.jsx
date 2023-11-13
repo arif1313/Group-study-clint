9df95ca133b7e19beb3150c2a2ec4c40c2a1a2fb
arif1313 <arifhossen13131@gmail.com>
2023-11-13 23:35:28 +0600

    load data succesfully use credentials none and server consol cook datta

--- src/Component/TakenAssignment/TakenAssignment.jsx
+++ src/Component/TakenAssignment/TakenAssignment.jsx
@@ -9,7 +9,7 @@ const TakenAssignment = () => {
     const { user } = useContext(AutContext);
     const [takeassignment, setTakenAssignment] = useState([]);
     useEffect(() => {
-        fetch(`http://localhost:5000/mytakenAssignment?email=${user?.email}`)
+        fetch(`http://localhost:5000/mytakenAssignment?email=${user?.email}`,{credentials:'include'})
             .then((res) => res.json())
             .then((data) => setTakenAssignment(data));
     }, [user]);
